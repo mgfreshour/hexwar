6e91db9a4137ed32467170172c097fba6853207b
M Freshour <mgfreshour@gmail.com>
2011-08-05 19:34:57 -0400

    Enable mailing in Prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,4 +46,5 @@ HexGame::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  config.action_mailer.delivery_method = :sendmail
 end
