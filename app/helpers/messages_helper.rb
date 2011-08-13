544c7bb92536e50c1876abbe230cb5aad01df781
M Freshour <mgfreshour@gmail.com>
2011-08-13 08:02:17 -0400

    Some tweaks to the game conversation UI

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -5,6 +5,8 @@ module MessagesHelper
     	ret += "messages += '[#{h msg.updated_at.strftime('%m/%d/%Y %H:%M %Z')}] #{h msg.player.name} : #{h(msg.text).gsub("\n", "<br />").gsub("\r",'').gsub("'", '`')}<br />';\n"
     end
     ret += "$('#conversation_entries').html(messages);"
+    ret += "$('#conversation_entries').prop({ scrollTop: $('#conversation_entries').prop('scrollHeight') });";
+    ret += "$('#message_text').val('');"
     
     ret.html_safe
   end
