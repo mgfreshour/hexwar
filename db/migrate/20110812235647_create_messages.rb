8e0211f3ba3aab8013da6673dbcb1ec3b8e5378d
M Freshour <mgfreshour@gmail.com>
2011-08-12 21:26:19 -0400

    MySql does not support defaults on Blob/Text types

--- db/migrate/20110812235647_create_messages.rb
+++ db/migrate/20110812235647_create_messages.rb
@@ -1,7 +1,7 @@
 class CreateMessages < ActiveRecord::Migration
   def self.up
     create_table :messages do |t|
-      t.text :text, :null=>false, :default=>'Message Text'
+      t.text :text, :null=>false
       t.string :title
       t.integer :player_id, :null=>false
       t.integer :game_id
