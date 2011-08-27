13b0d2eca4a095fcc045efbf896fb12145cfbe0e
M Freshour <mgfreshour@gmail.com>
2011-08-27 00:21:23 -0400

    cleaning up session controller a bit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,12 @@ class SessionsController < ApplicationController
   
   private
   def fb_login_user(token, user_id)
-    faceboook_graph = Koala::Facebook::GraphAPI.new(token)
-    profile = faceboook_graph.get_object("me")
-    
-    player = Player.find_by_provider_and_uid('facebook', user_id) || Player.create_with_omniauth('facebook', user_id, profile[:name])
+    player = Player.find_by_provider_and_uid('facebook', user_id)
+    if !player
+      faceboook_graph = Koala::Facebook::GraphAPI.new(token)
+      profile = faceboook_graph.get_object("me")
+      Player.create_with_omniauth('facebook', user_id, profile[:name])
+    end
 
     if player.token != token
       player.token = token
@@ -29,9 +31,8 @@ class SessionsController < ApplicationController
 
       if session[:signed_request]
         auth = oauth.parse_signed_request(session[:signed_request])
-        logger.debug auth.to_s
         if auth['user_id'] && auth[:oauth_token]
-          return self.fb_login_user(auth[:oauth_token], from_cookie[:user_id])
+          return fb_login_user(auth[:oauth_token], from_cookie[:user_id])
         end
       end
 
@@ -45,29 +46,29 @@ class SessionsController < ApplicationController
     end
   end
   
-  def create    
-    auth = request.env["omniauth.auth"]
-    
-    @faceboook_graph = Koala::Facebook::GraphAPI.new(auth['credentials']['token'])
-    
-    player = Player.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Player.create_with_omniauth(auth["provider"], auth["uid"], auth["user_info"]["name"])
-
-    if player.token != auth['credentials']['token']
-      player.token = auth['credentials']['token']
-      player.save
-    end
-    
-    session[:player_id] = player.id
-    session[:expires] = Time.now + 1.day
-
-    redirect_to root_url
-  end
-  
-  def failure
-    respond_to do |format|
-      format.html { render :text=>"Unable to log you in - #{params[:message]}"}
-    end
-  end
+  # def create    
+  #   auth = request.env["omniauth.auth"]
+  #   
+  #   @faceboook_graph = Koala::Facebook::GraphAPI.new(auth['credentials']['token'])
+  #   
+  #   player = Player.find_by_provider_and_uid(auth["provider"], auth["uid"]) || Player.create_with_omniauth(auth["provider"], auth["uid"], auth["user_info"]["name"])
+  # 
+  #   if player.token != auth['credentials']['token']
+  #     player.token = auth['credentials']['token']
+  #     player.save
+  #   end
+  #   
+  #   session[:player_id] = player.id
+  #   session[:expires] = Time.now + 1.day
+  # 
+  #   redirect_to root_url
+  # end
+  # 
+  # def failure
+  #   respond_to do |format|
+  #     format.html { render :text=>"Unable to log you in - #{params[:message]}"}
+  #   end
+  # end
   
   def destroy
     session[:player_id] = nil  
