17a6586668f39e033149b324d14a559448dc669c
M Freshour <mgfreshour@gmail.com>
2011-08-27 06:25:51 -0400

    Some clean up from changes in config files

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -1,18 +1,18 @@
 class Notifier < ActionMailer::Base
   default :from => "hexwar@mgfstudios.com"
+  @host = HexGame::Application.config.hexwar['host']
 
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
   #   en.notifier.notify_turn.subject
   #
-  def notify_turn (game_name, game_id, player_email, player_id, host='hexwar.mgfstudios.com')
+  def notify_turn (game_name, game_id, player_email, player_id)
     @game_name = game_name
     @game_id = game_id
     @player_email = player_email
     @player_id = player_id
-    @host = host
 
-    mail :to => player_email, :subject=>'Hexwar : It is your Turn on '+@game_name+'!!'
+    mail :to => player_email, :subject=>'Hexwar : It is your Turn!!'
   end
 end
