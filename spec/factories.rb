09a54acc8fae6c4d3d38ccdafa1e01dda1f23eaf
Morgan Freshour <mfreshour@crsinc.com>
2011-09-01 14:05:49 -0400

    Alpha sorting the models in the factories definitions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,8 @@
-Factory.define :player do |f|
-  f.name 'John Bon'
-  f.provider 'facebook'
-  f.sequence(:uid) { |n| "UID_#{n}" }
-  f.sequence(:email) { |n| "email#{n}@example.com" }
-  f.notify_by_email true
+
+Factory.define :game do |f|
+  f.name 'Test Game'
+  f.association :map
+  #f.game_players {|game_player| [game_player.association(:game_player)]}
 end
 
 Factory.define :game_player do |f|
@@ -15,23 +14,12 @@ Factory.define :game_turn do |f|
   f.association :game
 end
 
-Factory.define :game do |f|
-  f.name 'Test Game'
-  f.association :map
-  #f.game_players {|game_player| [game_player.association(:game_player)]}
-end
-
 Factory.define :map do |f|
   f.name 'Test Map'
   f.height 10
   f.width 10
   f.number_of_players 2
   f.unit_data ['test','me']
-end
-
-Factory.define :turn_notification do |f|
-  f.association :player
-  f.association :game
 end
 
 Factory.define :message do |f|
@@ -44,6 +32,14 @@ Factory.define :message_viewer do |f|
   f.association :player
 end
 
+Factory.define :player do |f|
+  f.name 'John Bon'
+  f.provider 'facebook'
+  f.sequence(:uid) { |n| "UID_#{n}" }
+  f.sequence(:email) { |n| "email#{n}@example.com" }
+  f.notify_by_email true
+end
+
 Factory.define :terrain_modifier do |f|
 
 end
@@ -51,6 +47,11 @@ end
 Factory.define :tile_type do |f|
   f.name 'test tile'
   f.img 'my.png'
+end
+
+Factory.define :turn_notification do |f|
+  f.association :player
+  f.association :game
 end
 
 Factory.define :unit_type do |f|
