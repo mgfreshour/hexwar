aa9628b3c44ad1ec9d07f400ad113a8d25ca22c6
M Freshour <mgfreshour@gmail.com>
2012-02-01 22:56:35 -0500

    More N+1 query fixes

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,7 +1,7 @@
 module MessagesHelper
   def populateGameConversationJS(game)
     ret = "var messages = '';"
-    game.messages.find(:all, :order => "created_at ASC").each do |msg|
+    game.messages.find(:all, :order => "created_at ASC", :include => [:player]).each do |msg|
     	ret += "messages += '[#{h msg.updated_at.strftime('%m/%d/%Y %H:%M %Z')}] #{h msg.player.name} : #{h(msg.text).gsub("\n", "<br />").gsub("\r",'').gsub("'", '`')}<br />';\n"
     end
     ret += "$('#conversation_entries').html(messages);"
